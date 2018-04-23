6b25537aff5461ab7284e2a67e8c2ab1d6eb2cec
Lukas Müller <lukas.mueller@virtual-identity.com>
2018-04-23 15:37:31 +0200

    set up the necessary files for extension to do something in chrome

--- background.js
+++ background.js
@@ -1,6 +1,27 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        id: 'main',
-        bounds: { width: 620, height:500 }
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({color: '#3aa757'}, function () {
+        console.log('the color is green');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules(
+            [
+                {
+                    conditions: [
+                        new chrome.declarativeContent.PageStateMatcher(
+                            {
+                                pageUrl:
+                                    {
+                                        hostEquals: 'developer.chrome.com'
+                                    }
+                            }
+                        )
+                    ],
+                    actions:
+                        [
+                            new chrome.declarativeContent.ShowPageAction()
+                        ]
+                }
+            ]
+        );
     });
 });
